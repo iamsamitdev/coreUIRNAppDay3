d17be8e99b61ff2b4fe3059958f90eb9151f5c85
Samit Koyom <samitkoyom@gmail.com>
2022-03-18 01:41:53 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,29 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import HomeScreen from './src/screens/HomeScreen'
+import GreetingScreen from './src/screens/GreetingScreen'
+import CounterScreen from './src/screens/CounterScreen';
+import SafeAreaScreen from './src/screens/SafeAreaScreen';
+import PlatformOS from './src/screens/PlatformOS';
+import Comment from './src/screens/comments/Comment';
+import ScrollViewScreen from './src/screens/ScrollViewScreen';
+import FlatListScreen from './src/screens/FlatListScreen';
+import FlatListWithSwiper from './src/screens/FlatListWithSwiper';
+import FlexDemoScreen from './src/screens/FlexDemoScreen';
+import LoginScreen from './src/screens/LoginScreen';
+
+const App = () => {
+  // return <HomeScreen />
+  // return <GreetingScreen />
+  // return <CounterScreen />
+  // return <SafeAreaScreen />
+  // return <PlatformOS />
+  // return <Comment />
+  // return <ScrollViewScreen />
+  // return <FlatListScreen />
+  // return <FlatListWithSwiper />
+  // return <FlexDemoScreen />
+  return <LoginScreen />
+}
+
+export default App
---
+++ src/screens/LoginScreen.js
@@ -0,0 +1,169 @@
+import React from 'react'
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  ImageBackground,
+  Dimensions,
+  TextInput,
+  Button,
+  TouchableOpacity,
+  ScrollView,
+  SafeAreaView,
+} from 'react-native'
+
+const { width, height } = Dimensions.get('screen')
+
+const LoginScreen = () => {
+
+  const background = require('../assets/images/login/login1_bg.png')
+  const mark = require('../assets/images/login/login1_mark.png')
+  const lockIcon = require('../assets/images/login/login1_lock.png')
+  const personIcon = require('../assets/images/login/login1_person.png')
+
+  return (
+    <SafeAreaView>
+      <ScrollView>
+      <ImageBackground
+        source={background}
+        style={styles.background}
+        resizeMode="cover">
+          <View style={styles.markWrap}>
+            <Image source={mark} style={styles.mark} resizeMode="contain" />
+          </View>
+          <View style={styles.wrapper}>
+            <View style={styles.inputWrap}>
+              <View style={styles.iconWrap}>
+                <Image
+                  source={personIcon}
+                  style={styles.icon}
+                  resizeMode="contain"
+                />
+              </View>
+              <TextInput
+                placeholder="ชื่อผู้ใช้"
+                placeholderTextColor="#FFFFFF"
+                style={styles.input}
+              />
+            </View>
+            <View style={styles.inputWrap}>
+              <View style={styles.iconWrap}>
+                <Image
+                  source={lockIcon}
+                  style={styles.icon}
+                  resizeMode="contain"
+                />
+              </View>
+              <TextInput
+                placeholderTextColor="#FFFFFF"
+                placeholder="รหัสผ่าน"
+                style={styles.input}
+                secureTextEntry
+              />
+            </View>
+            <TouchableOpacity activeOpacity={0.5}>
+              <View>
+                <Text style={styles.forgotPasswordText}>ลืมรหัสผ่าน ?</Text>
+              </View>
+            </TouchableOpacity>
+            <TouchableOpacity activeOpacity={0.5}>
+              <View style={styles.button}>
+                <Text style={styles.buttonText}>เข้าสู่ระบบ</Text>
+              </View>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.container}>
+            <View style={styles.signupWrap}>
+              <Text style={styles.accountText}>ยังไม่เป็นสมาชิก ?</Text>
+              <TouchableOpacity activeOpacity={0.5}>
+                <View>
+                  <Text style={styles.signupLinkText}>สมัครสมาชิก</Text>
+                </View>
+              </TouchableOpacity>
+            </View>
+          </View>
+      </ImageBackground>
+      </ScrollView>
+    </SafeAreaView>
+  )
+}
+
+export default LoginScreen
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  markWrap: {
+    flex: 1,
+    paddingVertical: 30,
+    alignSelf: 'center',
+  },
+  mark: {
+    width: 120,
+    height: 150,
+    flex: 1,
+  },
+  background: {
+    width,
+    height,
+  },
+  wrapper: {
+    paddingVertical: 30,
+  },
+  inputWrap: {
+    flexDirection: 'row',
+    marginVertical: 10,
+    height: 60,
+    borderBottomWidth: 1,
+    borderBottomColor: '#CCCCCC',
+  },
+  iconWrap: {
+    paddingHorizontal: 7,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  icon: {
+    height: 20,
+    width: 20,
+  },
+  input: {
+    fontSize: 20,
+    flex: 1,
+    paddingHorizontal: 10,
+    color: '#FFFFFF',
+  },
+  button: {
+    backgroundColor: '#FF3366',
+    paddingVertical: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 30,
+  },
+  buttonText: {
+    color: '#FFFFFF',
+    fontSize: 24,
+  },
+  forgotPasswordText: {
+    color: '#D8D8D8',
+    backgroundColor: 'transparent',
+    textAlign: 'right',
+    paddingRight: 15,
+    fontSize: 18,
+  },
+  signupWrap: {
+    backgroundColor: 'transparent',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  accountText: {
+    fontSize: 16,
+    color: '#D8D8D8',
+  },
+  signupLinkText: {
+    color: '#FFFFFF',
+    marginLeft: 5,
+  },
+})
